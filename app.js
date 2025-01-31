58b18d948bb8fcca1ac32cf58d4310158c0e7a3d
ankit-3131 <heyyynkit@gmail.com>
2025-01-31 11:01:40 +0530

    View Engine Set and homepage rendered

--- app.js
+++ app.js
@@ -8,11 +8,17 @@ connectDB();
 const app = express();
 const PORT = process.env.port || 3000;
 
+// app.use(express.urlencoded({extended: true}));
+// app.use(cookieParser());
+
+app.set('view engine', 'ejs');
+app.set('views', './views');
+app.use(express.static('public'));
 
 app.get('/', async (req, res)=>{
-    res.send('Hello World');
+    res.render('home');
 })
 
 app.listen(PORT, ()=>{
-    console.log(`Server is running on port ${PORT}`);
+    console.log(`Server is running at http://localhost:${PORT}/`);
 })
