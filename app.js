5d407b849a4e09e32f8abc29553d9f19ccbd7d1b
ankit-3131 <heyyynkit@gmail.com>
2025-02-06 23:11:22 +0530

    Added Health Bot and updated its training datasets

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ import dotenv from 'dotenv'
 dotenv.config();
 import cookieParser from 'cookie-parser';
 
+import run from './services/healthBot.js'
 import userRoute from './routes/userRouter.js';
 import doctorRoute from './routes/doctorRoute.js';
 import connectDB from './controllers/configDB.js';
@@ -16,6 +17,7 @@ app.set('view engine', 'ejs');
 app.set('views', './views');
 app.use(express.static('public'));
 app.use(express.urlencoded({extended: true}));
+app.use(express.json());
 app.use(cookieParser());
 app.use(express.static('public'));
 
@@ -26,6 +28,18 @@ app.get('/', async (req, res)=>{
       res.clearCookie('content');
     res.render('home', {flag, content});
 })
+
+app.post('/api/run', async (req, res) => {
+    const { input } = req.body; // Get user input from the request body
+
+    try {
+        const result = await run(input); // Call the run function with user input
+        res.json({ output: result }); // Send the result back to the client
+    } catch (error) {
+        console.error('Error running the health bot:', error);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+});
 
 app.use('/user',userRoute);
 app.use('/doctor',doctorRoute);
